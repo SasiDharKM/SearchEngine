f8efe7659bb7ae99a130602f833b644251c40167
SasiDharKM <sasiboss.rock@gmail.com>
2016-12-23 15:24:23 +0530

    Added crawler code

--- link_grabber.py
+++ link_grabber.py
@@ -13,15 +13,30 @@ def get_next_target(page):
     start_quote = page.find('"', start_link)
     end_quote = page.find('"', start_quote + 1)
     url = page[start_quote + 1:end_quote]
-    
     return url, end_quote
 
-def print_all_links(page):
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+            
+def get_all_links(page):
+    links=[]
     while True:
         url,endpos =get_next_target(page)
         if url:
-            print url
+            links.append(url)
             page=page[endpos:]
         else:
             break
+    return links    
 
+def crawl_web(seed):
+    tocrawl=[seed]
+    crawled=[]
+    while tocrawl:
+        page=tocrawl.pop()
+        if page not in crawled:
+            union(tocrawl,get_all_links(get_page(page)))
+            crawled.append(page)
+    return crawled        
